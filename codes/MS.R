f33f68b333fb446cff8db63afb3eee0b301c8276
Gustavo Vital <gustavovital@id.uff.br>
2025-04-26 01:08:31 +0100

    update and add MS code in R

---
+++ codes/MS.R
@@ -0,0 +1,80 @@
+# Packages
+library(tidyverse)
+library(lubridate)
+library(corrplot)
+library(vars)
+library(MSwM)
+library(zoo)
+
+setwd("~/Documents/GitHub/PhDEcon107/codes/")
+
+icc <- read.csv2("~/Documents/GitHub/PhDEcon107/data/icc.csv")
+icc <- icc %>%
+  mutate(DATE = floor_date(as.Date(DATE), unit = "month"))
+
+sentiment_scored_aggregated <- read.csv("~/Documents/GitHub/PhDEcon107/data/sentiment_scored_aggregated.csv")
+
+sentiment_scored_aggregated$DATE <- as.Date(sentiment_scored_aggregated$DATE)
+
+# Agrupar e calcular sentimentos ponderados
+data <- sentiment_scored_aggregated %>%
+  mutate(DATE = floor_date(DATE, unit = "month")) %>%
+  group_by(DATE) %>%
+  summarise(
+    weighted_sentiment_finbert = sum(sentiment_finbert * LEN, na.rm = TRUE) / sum(LEN, na.rm = TRUE),
+    weighted_sentiment_yiyanghkust = sum(sentiment_yiyanghkust * LEN, na.rm = TRUE) / sum(LEN, na.rm = TRUE),
+    total_length = sum(LEN, na.rm = TRUE),
+    n_docs = n()
+  ) %>%
+  ungroup() %>%
+  mutate(
+    norm_finbert = (weighted_sentiment_finbert - min(weighted_sentiment_finbert)) /
+      (max(weighted_sentiment_finbert) - min(weighted_sentiment_finbert)),
+    norm_yiyanghkust = (weighted_sentiment_yiyanghkust - min(weighted_sentiment_yiyanghkust)) /
+      (max(weighted_sentiment_yiyanghkust) - min(weighted_sentiment_yiyanghkust)),
+    mean_weighted_sentiment = (weighted_sentiment_finbert + weighted_sentiment_yiyanghkust) / 2,
+    norm_mean_sentiment = (mean_weighted_sentiment - min(mean_weighted_sentiment)) /
+      (max(mean_weighted_sentiment) - min(mean_weighted_sentiment))
+  )
+
+data$DATE <- as.Date(data$DATE)
+icc$DATE <- as.Date(icc$DATE)
+
+data <- left_join(data, icc, by = "DATE")
+
+for (i in 2:6) {
+  new_colname <- paste0(names(data)[2], "_", i)
+  new_colname2 <- paste0(names(data)[3], "_", i)
+  new_colname6 <- paste0(names(data)[6], "_", i)
+  new_colname7 <- paste0(names(data)[7], "_", i)
+  new_colname8 <- paste0(names(data)[9], "_", i)
+  data[[new_colname]] <- zoo::rollmean(data$weighted_sentiment_finbert, k = i, fill = NA, align = "right")
+  data[[new_colname2]] <- zoo::rollmean(data$weighted_sentiment_yiyanghkust, k = i, fill = NA, align = "right")
+  data[[new_colname6]] <- zoo::rollmean(data$norm_finbert, k = i, fill = NA, align = "right")
+  data[[new_colname7]] <- zoo::rollmean(data$norm_yiyanghkust, k = i, fill = NA, align = "right")
+  data[[new_colname8]] <- zoo::rollmean(data$norm_mean_sentiment, k = i, fill = NA, align = "right")
+}
+
+data2 <- data %>%
+  filter(DATE < as.Date('2025-01-01') & DATE >= as.Date('2020-01-01')) %>% 
+  mutate(covid = ifelse(DATE < as.Date('2021-07-01') & DATE > as.Date('2020-03-01'), 1, 0)) %>% 
+  na.omit() 
+
+plot(data2$ICC, type = 'l')
+plot(data2$norm_mean_sentiment_4, type = 'l')
+
+data2$icc_diff <- c(NA, diff(data2$ICC))
+
+lm_model <- lm(ICC ~ norm_mean_sentiment_4, data = data2, na.action = na.exclude)
+summary(lm_model)
+
+ms_model <- msmFit(lm_model,
+                   k = 2,
+                   p = 0,
+                   sw = c( TRUE, TRUE, TRUE),
+                   control = list(parallel = FALSE, trace = TRUE))
+
+summary(ms_model)
+# plot(resid(ms_model@model), type='l')
+
+plotProb(ms_model, which = c(3))
--- codes/ecb_plus.py
+++ codes/ecb_plus.py
@@ -3,7 +3,7 @@ import pandas as pd
 corpus = pd.read_csv('/Users/gustavovital/Documents/GitHub/PhDEcon107/data/ecb_speech_corpus_clean.csv')
 score = pd.read_csv('/Users/gustavovital/Documents/GitHub/PhDEcon107/data/sentiment_scored_aggregated.csv')
 
-score['length'] = corpus['TEXT'].str.len()
+score['LEN'] = corpus['TEXT'].str.len()
 score['relative_finbert'] = score['sentiment_finbert'] * score['length']
 score['relative_yiyanghkust'] = score['sentiment_yiyanghkust'] * score['length']
 
